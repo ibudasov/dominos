d9a17c018f33fa15fc2dbe108bf0e5ab37858edc
Igor <igor.budasov@gmail.com>
2018-03-16 17:33:57 +0100

    leading and trailing number can be retrieved from the board

--- src/Domain/Board.php
+++ src/Domain/Board.php
@@ -8,16 +8,69 @@ class Board
 {
     /** @var array */
     private $tiles;
+    /** @var int */
+    private $leadingNumber;
+    /** @var int */
+    private $trailingNumber;
 
     public function __construct()
     {
         $this->tiles = [];
+        $this->leadingNumber = 0;
+        $this->trailingNumber = 0;
     }
 
     public function addTile(Tile $tile): int
     {
+        $this->setLeadingAndTrailingNumberWhenBoardsIsEmpty($tile);
+
+        $this->updateLeadingNumber($tile);
+
+        $this->updateTrailingNumber($tile);
+
         $this->tiles[] = $tile;
 
         return \count($this->tiles);
+    }
+
+    public function getLeadingNumber(): int
+    {
+        return $this->leadingNumber;
+    }
+
+    public function getTrailingNumber(): int
+    {
+        return $this->trailingNumber;
+    }
+
+    /**
+     * @param Tile $tile
+     */
+    private function setLeadingAndTrailingNumberWhenBoardsIsEmpty(Tile $tile): void
+    {
+        if (0 === \count($this->tiles)) {
+            $this->leadingNumber = $tile->getFirstValue();
+            $this->trailingNumber = $tile->getSecondValue();
+        }
+    }
+
+    /**
+     * @param Tile $tile
+     */
+    private function updateLeadingNumber(Tile $tile): void
+    {
+        $this->leadingNumber = ($tile->getSecondValue() === $this->leadingNumber)
+            ? $tile->getFirstValue()
+            : $this->leadingNumber;
+    }
+
+    /**
+     * @param Tile $tile
+     */
+    private function updateTrailingNumber(Tile $tile): void
+    {
+        $this->trailingNumber = ($tile->getFirstValue() === $this->trailingNumber)
+            ? $tile->getSecondValue()
+            : $this->trailingNumber;
     }
 }
--- tests/Domain/BoardTest.php
+++ tests/Domain/BoardTest.php
@@ -13,7 +13,49 @@ class BoardTest extends TestCase
         $board = new Board();
 
         $tileMock = \Mockery::mock(Tile::class);
+        $tileMock->shouldReceive('getFirstValue')
+            ->once()
+            ->andReturn(6);
+        $tileMock->shouldReceive('getSecondValue')
+            ->once()
+            ->andReturn(5);
 
         self::assertEquals(1, $board->addTile($tileMock));
+    }
+
+    public function testThatLeadingAndTrailingNumbersCanBeRetrieved(): void
+    {
+        $board = new Board();
+
+        $tileMock = \Mockery::mock(Tile::class);
+        $tileMock->shouldReceive('getFirstValue')
+            ->once()
+            ->andReturn(4);
+        $tileMock->shouldReceive('getSecondValue')
+            ->once()
+            ->andReturn(3);
+
+        $tileMock2 = \Mockery::mock(Tile::class);
+        $tileMock2->shouldReceive('getFirstValue')
+            ->once()
+            ->andReturn(5);
+        $tileMock2->shouldReceive('getSecondValue')
+            ->once()
+            ->andReturn(4);
+
+        $tileMock3 = \Mockery::mock(Tile::class);
+        $tileMock3->shouldReceive('getFirstValue')
+            ->once()
+            ->andReturn(3);
+        $tileMock3->shouldReceive('getSecondValue')
+            ->once()
+            ->andReturn(6);
+
+        $board->addTile($tileMock);
+        $board->addTile($tileMock2);
+        $board->addTile($tileMock3);
+
+        self::assertEquals(5, $board->getLeadingNumber());
+        self::assertEquals(6, $board->getTrailingNumber());
     }
 }
